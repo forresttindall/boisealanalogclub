9801dd6a433ee6d514b40e68aa26f4739917f3ec
forresttindall <forrest.tindall@gmail.com>
2025-08-21 16:50:47 -0600

    color change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -69,7 +69,7 @@ function Home() {
           <div className="home-gallery-grid">
             {[
               { src: '/Images/AD91D45A-C6F3-4947-AE86-45D98A009ED6.JPG', photographer: '@mcgillis_captures' },
-              { src: '/Images/17276 08072025 4-013.JPG', photographer: '@forrest.tindall' },
+              { src: '/Images/1.jpg', photographer: '@forrest.tindall' },
               { src: '/Images/IMG_0842.JPG', photographer: '@aidan.cleve' },
               { src: '/Images/IMG_0919.JPG', photographer: '@exhausted_imagery' },
               { src: '/Images/EA527AD7-6AE5-4797-9ABA-7BED5A35EEED.JPG', photographer: '@mcgillis_captures' },
@@ -78,7 +78,7 @@ function Home() {
               { src: '/Images/4921C783-EBD7-4332-8099-C55CFE43D956.JPG', photographer: '@mcgillis_captures' },
               { src: '/Images/05D45C51-66EA-4AC0-9D06-1A23B744C9CC.JPG', photographer: '@forrest.tindall' },
               { src: '/Images/IMG_0920.JPG', photographer: '@exhausted_imagery' },
-              { src: '/Images/img20250804_13240186.JPG', photographer: '@forrest.tindall' },
+              { src: '/Images/5.jpg', photographer: '@forrest.tindall' },
             ].map((image, index) => (
               <div
                 className="home-gallery-item"
